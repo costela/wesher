b0bfeb1f8e42b95ab82ea854b98e5441e43e719e
kaiyou <pierre@jaury.eu>
2020-05-06 19:38:49 +0200

    Move the metadata parsing code to node.go

--- main.go
+++ main.go
@@ -66,18 +66,7 @@ func main() {
 	for {
 		select {
 		case rawNodes := <-nodec:
-			logrus.Info("cluster members:\n")
-			nodes := make([]node, 0)
-			for _, node := range rawNodes {
-				meta, err := decodeNodeMeta(node.Meta)
-				if err != nil {
-					logrus.Warnf("\t addr: %s, could not decode metadata", node.Addr)
-					continue
-				}
-				node.nodeMeta = meta
-				nodes = append(nodes, node)
-				logrus.Infof("\taddr: %s, overlay: %s, pubkey: %s", node.Addr, node.OverlayAddr, node.PubKey)
-			}
+			nodes := parseNodesMeta(rawNodes)
 			if err := wg.setUpInterface(nodes); err != nil {
 				logrus.WithError(err).Error("could not up interface")
 				wg.downInterface()
