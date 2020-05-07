9fdab652375ffa98af749b59d4b751d0d84806b8
kaiyou <pierre@jaury.eu>
2020-05-07 09:58:31 +0200

    Revert "Move the metadata parsing code to node.go"
    This reverts commit ac6289b5bf95e8903ea90ddf807832a2358371b0.

--- main.go
+++ main.go
@@ -66,7 +66,18 @@ func main() {
 	for {
 		select {
 		case rawNodes := <-nodec:
-			nodes := parseNodesMeta(rawNodes)
+			logrus.Info("cluster members:\n")
+			nodes := make([]node, 0, len(rawNodes))
+			for _, node := range rawNodes {
+				meta, err := decodeNodeMeta(node.Meta)
+				if err != nil {
+					logrus.Warnf("\t addr: %s, could not decode metadata", node.Addr)
+					continue
+				}
+				node.nodeMeta = meta
+				nodes = append(nodes, node)
+				logrus.Infof("\taddr: %s, overlay: %s, pubkey: %s", node.Addr, node.OverlayAddr, node.PubKey)
+			}
 			if err := wg.setUpInterface(nodes); err != nil {
 				logrus.WithError(err).Error("could not up interface")
 				wg.downInterface()
